732a2558f95631febf76261cd286945bc8e5f55b
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-01-14 19:44:24 +0300

    Update omni_S11.mk

--- omni_S11.mk
+++ omni_S11.mk
@@ -1,13 +1,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
 # Otherwise, If you have 32-bit device, add the below line instead of above line
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
 # Another common config inclusion
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
 # If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from S11 device
+$(call inherit-product, device/ITEL/S11/device.mk)
+
 # Replace $$DEVICE$$ with your Device Name's Value.
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
 PRODUCT_COPY_FILES += device/ITEL/S11/prebuilt/zImage:kernel
+
 # Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
 # PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
 PRODUCT_DEVICE := S11
@@ -17,7 +25,8 @@ PRODUCT_MODEL := ITEL S11
 PRODUCT_MANUFACTURER := ITEL
 # Forcefully add mtp support (adb is already there)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config=mtp
+   persist.sys.usb.config=mtp
+
 # Add fingerprint from Stock ROM build.prop PRODUCT_BUILD_PROP_OVERRIDES += \
 # These lines are from my device. You MUST Replace yours.
 BUILD_FINGERPRINT="Itel/S11/S11:6.0/MRA58K/S11-I801-6.0-V070-180420:user/release-keys" \
