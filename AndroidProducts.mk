64aa7af4e49c35686388b733c05742a323a7fffc
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-01-14 18:52:22 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,4 @@
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
 # The part of last line in mine looks like "omni_Primo_RX5.mk"
 LOCAL_PATH := device/ITEL/S11
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_ITEL_S11.mk
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_S11.mk
