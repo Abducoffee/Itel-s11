b5fe0ebae4c75b6468ab43175673418b2a0b7f30
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2024-01-14 21:17:02 +0300

    Update omni_S11.mk

--- omni_S11.mk
+++ omni_S11.mk
@@ -1,33 +1,28 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Another common config inclusion
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
-
-# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from S11 device
-$(call inherit-product, device/ITEL/S11/device.mk)
+$(call inherit-product, device/itel/S11/device.mk)
 
-# Replace $$DEVICE$$ with your Device Name's Value.
-# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
-PRODUCT_COPY_FILES += device/ITEL/S11/prebuilt/zImage:kernel
-
-# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
-# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
 PRODUCT_DEVICE := S11
 PRODUCT_NAME := omni_S11
-PRODUCT_BRAND := ITEL
-PRODUCT_MODEL := ITEL S11
-PRODUCT_MANUFACTURER := ITEL
-# Forcefully add mtp support (adb is already there)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-   persist.sys.usb.config=mtp
+PRODUCT_BRAND := Itel
+PRODUCT_MODEL := Itel S11
+PRODUCT_MANUFACTURER := Itel
 
-# Add fingerprint from Stock ROM build.prop PRODUCT_BUILD_PROP_OVERRIDES += \
-# These lines are from my device. You MUST Replace yours.
-BUILD_FINGERPRINT="Itel/S11/S11:6.0/MRA58K/S11-I801-6.0-V070-180420:user/release-keys" \
-PRIVATE_BUILD_DESC="S11-user 6.0 MRA58K 1524217265 release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="S11-user 6.0 MRA58K 1524217265 release-keys"
+
+BUILD_FINGERPRINT := Itel/S11/S11:6.0/MRA58K/S11-I801-6.0-V070-180420:user/release-keys
